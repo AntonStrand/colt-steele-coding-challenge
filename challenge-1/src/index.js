80d8fb0182f82124b2658c419f060a7e6a764432
antonstrand <jag@antonstrand.se>
2020-02-22 18:23:48 +0100

    solution for challenge 1

---
+++ challenge-1/src/index.js
@@ -0,0 +1,90 @@
+'use strict'
+
+/** HELPERS */
+/** query :: String -> HTMLElement -> HTMLElement */
+const query = query => element => element.querySelector(query)
+
+/** queryAll :: String -> HTMLElement -> [HTMLElement] */
+const queryAll = query => element => Array.from(element.querySelectorAll(query))
+
+/** getActiveStep :: () -> HTMLElement -> HTMLElement */
+const getActiveStep = () => query('.active')(window.document)
+
+/** getSteps :: () -> HTMLElement -> [HTMLElement] */
+const getSteps = () => queryAll('.step-form > .step')(window.document)
+
+/** getInput :: HTMLElement -> HTMLElement */
+const getInput = query('input')
+
+/** getAllInput :: () -> [HTMLElement] */
+const getAllInputs = () => queryAll('input')(window.document)
+
+/** isConfirmStep :: HTMLElement -> Boolean */
+const isConfirmStep = step => step.getAttribute('id') === 'confirm'
+
+/** isActive :: HTMLElement -> Boolean */
+const isActive = element =>
+  Boolean(element.getAttribute('class').match(/active/))
+
+/** setActive :: Number -> () -> undefined */
+const setActive = direction => () => {
+  const steps = getSteps()
+  const activeIndex = steps.findIndex(isActive)
+
+  steps.forEach((step, i) => {
+    const classes = step.getAttribute('class')
+    i === activeIndex + direction
+      ? step.setAttribute('class', `${classes} active`)
+      : step.setAttribute('class', classes.replace('active', ''))
+  })
+  const stepsFirstInput = getInput(getActiveStep())
+  if (stepsFirstInput != null) {
+    stepsFirstInput.focus()
+  }
+}
+
+/** setNextActive :: () -> undefined */
+const setNextActive = setActive(1)
+
+/** setPrevActive :: () -> undefined */
+const setPrevActive = setActive(-1)
+
+/** handleActiveStep :: () -> undefined */
+const handleActiveStep = () => {
+  const active = getActiveStep()
+  const submitBtn = query('button[type="submit"]')(active)
+  const prevBtn = query('button[type="reset"]')(active)
+
+  const handleEvent = effect => e => {
+    e.preventDefault()
+    submitBtn.removeEventListener('click', onSubmit)
+    prevBtn && prevBtn.removeEventListener('click', onPrev)
+    effect()
+    handleActiveStep()
+  }
+
+  const onSubmit = handleEvent(setNextActive)
+  const onPrev = handleEvent(setPrevActive)
+
+  if (isConfirmStep(active)) {
+    queryAll('.value')(active).forEach(
+      (field, i) => (field.innerText = getAllInputs()[i].value)
+    )
+    const submitForm = handleEvent(() =>
+      console.log(
+        'Form:',
+        getAllInputs().reduce(
+          (formData, field) => ({ ...formData, [field.name]: field.value }),
+          {}
+        )
+      )
+    )
+
+    submitBtn.addEventListener('click', submitForm)
+  } else {
+    submitBtn.addEventListener('click', onSubmit)
+  }
+  prevBtn && prevBtn.addEventListener('click', onPrev)
+}
+
+handleActiveStep()
